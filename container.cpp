fc6f0f18e6fec6f7a6de671304771972f3eb4efb
sanupama <37939933+sanupama@users.noreply.github.com>
2018-09-07 19:13:16 +0530

    Add files via upload

---
+++ container.cpp
@@ -0,0 +1,99 @@
+
+#include<iostream>
+#include <set>
+#include<algorithm>
+#include<vector>
+void fun(int n){
+  int arr[n];
+}
+using namespace std;
+
+int main () {
+  int *i, y;
+  cin >> y;
+  std::vector<int> arr={1, 2, 3, 4, 5};
+/* To check wheather the container is empty or not */
+
+if (arr.empty()){
+    cout << "True";
+}
+else {
+    cout << "False";
+  
+  }
+
+  /* To insert an element at the end of vector */
+  arr.push_back(5);
+  arr.push_back(6);
+  arr.push_back(7);
+
+
+}
+
+/* To insert an element in the beginning in the vector */
+vector<int> arr;
+
+for(int i = 0; i < n; i++)
+    arr.insert(arr.begin(),i); // Inserts the value i at the start of the array 10 times.
+
+for(vector<int>::iterator it=arr.begin(); it<arr.end(); it++)
+    cout << *it; //Outputs the values of MyVector in order 
+
+/* To insert an element at a given index  in the vector */
+// vec1 and 2 have the same length & filled in somehow
+anothervector;
+
+arr = anothervector[i] // insert vec2[i] at position i of vec1
+
+
+arr<int>::iterator iterator = arr.begin();
+
+arr[n].insert(arr.begin()+i, anothervector[i]);
+
+
+
+  /* To remove the last element from the vector and return the same */
+arr.pop_back();
+
+vector<int>::iterator it;
+
+for(it = arr.begin(); it!= arr.end(); it++){
+    cout << *it <<" ";
+}
+
+/* To remove the first element from the vector and return the same*/
+arr.pop_back();
+
+vector<int>::iterator it;
+
+for(it = arr.end(); it!= arr.begin(); it++){
+    cout << *it <<" ";
+}
+
+/* To remove the element at a given index from the vector */
+arr.pop_back();
+
+vector<int>::iterator it;
+
+for(it = arr.begin(); it!= arr.end(); it++){
+    cout << *it <<" ";
+}
+
+  /* To return the last element of the vector */
+  arr[n].front();
+  cout << *i <<" ";
+
+  /* To return the first element of the vector */
+  arr[n].back();
+  cout << *i <<" ";
+
+
+/* To return the element at a a given position in the vector */
+arr.at(y);
+cout << y <<" ";
+
+
+/* To return the size of the vector or the number of elements in the vector */
+std::cout << "0. size: " << arr.size() << '\n';
+
+
